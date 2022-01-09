f0a67370080eef4d933c81f09a4ed998357cddf7
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-09 14:57:37 +0100

    finished routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
-  return <div>Home Page!</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Home Page!</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Products</a>
+      </Link>
+      <button onClick={() => router.push("/product")}>place order</button>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,8 +1,21 @@
-const ProductList = () => {
+import Link from "next/link";
+
+const ProductList = ({ productId = 100 }) => {
   return (
     <div>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
+      <h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h1>
+      <h1>Product List</h1>
+      <Link href={`/product/${[productId]}`}>
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/product/3" replace>
+        <h2>Product 2</h2>
+      </Link>
+
       <h2>Product 3</h2>
     </div>
   );
