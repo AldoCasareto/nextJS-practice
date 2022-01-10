9ab5dbdb0f80c007a3491dcc10f461376af43108
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-10 13:43:39 +0100

    static paths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -10,13 +10,19 @@ const Post = ({ posts }) => {
 
 export default Post;
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
   return {
-    paths: [
-      { params: { postId: "1" } },
-      { params: { postId: "2" } },
-      { params: { postId: "3" } },
-    ],
+    paths,
     fallback: false,
   };
 }
